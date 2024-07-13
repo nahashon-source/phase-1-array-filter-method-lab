f051b8a92c7329e4cc7fb838a889b83de46fb8d5
nahashon <nashonmwendwa0@gmail.com>
2024-07-13 10:15:34 +0300

    array filter method

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // Code your solution here
+function findMatching(drivers, str) {
+    return drivers.filter(driver => driver.toLowerCase() === str.toLowerCase());
+}
+
+function fuzzyMatch(drivers, str) {
+    return drivers.filter(driver => driver.toLowerCase().startsWith(str.toLowerCase()));
+}
+
+function matchName(drivers, str) {
+    return drivers.filter(driver => driver.name.toLowerCase() === str.toLowerCase());
+}
